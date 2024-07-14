fab359db6cb015bb1414565a0e3bf9278814b72e
houseontherivr <wertyboom911@hotmail.com>
2024-07-14 16:58:29 -0500

    added working row shift
    added working rowshift, just need to work on moving the frame

--- Assets/scripts/BOARD/Frame.cs
+++ Assets/scripts/BOARD/Frame.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Frame : MonoBehaviour
@@ -11,9 +12,10 @@ public class Frame : MonoBehaviour
     public GameObject body;
     public int numBody;
     RectTransform rectTransform;
-    private int interval = 1;
+    private int interval = 2;
     Vector3 anchor;
 
+    public List<GameObject> rows;
     public void Start()
     {
         rectTransform = GetComponent<RectTransform>();
@@ -21,6 +23,7 @@ public class Frame : MonoBehaviour
         for (int i = 0; i < numBody; i++)
         {
             GameObject img = Instantiate(body, new Vector3(0, 0, -1), Quaternion.identity, frame.transform);
+            rows.Add(img);
             //img.GetComponent<FlexGridRow>().parent = frame;
         }
     }
@@ -32,6 +35,21 @@ public class Frame : MonoBehaviour
         {
             interval = Mathf.FloorToInt(Time.time) + 1;
             rectTransform.localPosition = anchor;
+
+            shiftRows();
         }
+    }
+
+    public void shiftRows()
+    {
+        GameObject last = rows[rows.Count - 1];
+        Vector3 lcl = last.transform.localPosition;
+        for(int i = rows.Count - 2; i > -1; i--)
+        {
+            Vector3 slcl = rows[i].transform.localPosition;
+            rows[i].transform.localPosition = lcl;
+            lcl = slcl;
+        }
+        last.transform.localPosition = lcl;
     }
 }
