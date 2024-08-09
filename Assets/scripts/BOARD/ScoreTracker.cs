435342f275635b0f7944eda71ad4286e1ab1dbd5
ihatesummerandnoon <bakingpocket@gmail.com>
2024-08-09 16:07:13 -0500

    changed scoretracker

--- Assets/scripts/BOARD/ScoreTracker.cs
+++ Assets/scripts/BOARD/ScoreTracker.cs
@@ -16,7 +16,7 @@ public class ScoreTracker : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         scoreText.text = "SCORE: " + scoreCount;
         scoreCount++;
